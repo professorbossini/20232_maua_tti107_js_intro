ba7f400dc6ac0d417437d582a257d14eb9b75157
Rodrigo Bossini <professorbossini@gmail.com>
2023-11-09 11:45:19 -0300

    fim do exercicio async/await

--- previsao_do_tempo_async_await.js
+++ previsao_do_tempo_async_await.js
@@ -14,3 +14,18 @@ const {
 const url = `${PROTOCOL}://${URL_BASE}?q=${Q}&units=${UNITS}&appid=${APPID}&lang=${MY_LANGUAGE}&cnt=${CNT}`
 
 //Utilize async/await para iterar sobre a coleção de previsões, exibindo temp_min, temp_max, humidity, description
+
+const obterPrevisoes = async () => {
+  // const data = (await axios.get(url)).data
+  const { list } = (await axios.get(url)).data
+  for(let previsao of list){
+    const dt = new Date(+(previsao.dt) * 1000)
+    console.log(`Data: ${dt.toLocaleTimeString()}`)
+    console.log(`Temp min: ${previsao.main['temp_min']}`)
+    console.log(`Temp max: ${previsao['main'].temp_max}`)
+    console.log(`Humidity: ${previsao['main']['humidity']}`)
+    console.log(`Desc: ${previsao['weather'][0]['description']}`)
+    console.log('*****************************')
+  }
+}
+obterPrevisoes()
