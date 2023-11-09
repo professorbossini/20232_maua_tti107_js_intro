0643b694585d70bf2305ff0ce448cabcdbc7c47c
Rodrigo Bossini <professorbossini@gmail.com>
2023-11-09 10:10:49 -0300

    .env (nao faca isso)

--- previsao_do_tempo.js
+++ previsao_do_tempo.js
@@ -23,6 +23,7 @@ const {
 //https://api.openweathermap.org/data/2.5/forecast?q=Itu&units=metric&appid=chave&lang=pt_br&cnt=10
 
 const url = `${PROTOCOL}://${URL_BASE}?q=${Q}&units=${UNITS}&appid=${APPID}&lang=${LANGUAGE}&cnt=${CNT}`
+console.log(url)
 
 const minhaPromise = axios.get(url)
 
@@ -52,12 +53,22 @@ minhaPromise
   //depois acessar a propriedade temp_min
   console.log(res[0].main['temp_min'])
   return res
-}).
-.then((res) => {
+})
+.then((previsoes) => {
   //desafio para a aula do dia 09/11
   //o professor vai olhar o caderno
   //iterar sobre a coleção de previsões, exibindo temperatura minima, maxima, umidade relativa do ar, descrição de cada previsão
   // for..
+  for (let previsao of previsoes){
+    console.log('*************************')
+    console.log(`Temp min: ${previsao.main.temp_min}`)
+    console.log(`Temp max: ${previsao.main.temp_max}`)
+    console.log(`Umidade: ${previsao.main.humidity}`)
+    console.log(`Desc: ${previsao.weather[0].description}`)
+  }
 })
+// .then(res => console.log(res))
+
+//async/await
 
 
